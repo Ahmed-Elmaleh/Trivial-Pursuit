87ded3f288cc3b067299610b661f6155b9b1bb28
HuyNguyen0117 <huyng1@yahoo.com>
2020-07-14 22:44:20 -0700

    Update

--- src/Game.java
+++ src/Game.java
@@ -1,92 +1,91 @@
 
-import java.awt.EventQueue;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.LinkedList;
-
-
-public class Game {
-   
-    public static void main(String[] args) {
-    	
-    	Game newGame = new Game();
-    	newGame.run();
-    	
-    }
-    
-    
-    public void run() {
-    	
-
-    	/**
-         * Launch the application.
-         */
-    	EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					createAndShowGUI();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-    	
-    	
-    	
-        LinkedList<Question> eventQuestions = new LinkedList<Question>();
-        LinkedList<Question> peopleQuestions = new LinkedList<Question>();
-        LinkedList<Question> placesQuestions = new LinkedList<Question>();
-        LinkedList<Question> independenceDayHolidayQuestions = new LinkedList<Question>();
-        
-        
-
-    }
-
-    public void createAndShowGUI() throws Exception {
- 	    View view = new View();
-        Controller c = new Controller(view);
-        c.runController();
- 	}
- 	
-  
-    
-    
-
-	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
-    private void getQuestions(LinkedList<Question> list, String filePath)
-    {
-        String line="";
-        try
-        {
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
-            while ((line = br.readLine()) != null)
-            {
-                String[] data = line.split(",");
-
-                String[] answers = {data[1], data[2],data[3],data[4]};
-                Question q = new Question(data[0], answers, data[5]);
-                list.add(q);
-            }
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    
-	
-}
+//import java.awt.EventQueue;
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.LinkedList;
+//
+//
+//public class Game {
+//   
+//    public static void main(String[] args) {
+//    	
+//    	Game newGame = new Game();
+//    	newGame.run();
+//    	
+//    }
+//    
+//    
+//    public void run() {
+//    	
+//
+//    	/**
+//         * Launch the application.
+//         */
+//    	EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					createAndShowGUI();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//    	
+//    	
+//    	
+//        LinkedList<Question> eventQuestions = new LinkedList<Question>();
+//        LinkedList<Question> peopleQuestions = new LinkedList<Question>();
+//        LinkedList<Question> placesQuestions = new LinkedList<Question>();
+//        LinkedList<Question> independenceDayHolidayQuestions = new LinkedList<Question>();
+//        
+//        
+//
+//    }
+//
+//    public void createAndShowGUI() throws Exception {
+// 	    View view = new View();
+//        Controller c = new Controller(view);
+// 	}
+// 	
+//  
+//    
+//    
+//
+//	
+// 	
+// 	
+// 	
+// 	
+// 	
+// 	
+// 	
+//    private void getQuestions(LinkedList<Question> list, String filePath)
+//    {
+//        String line="";
+//        try
+//        {
+//            BufferedReader br = new BufferedReader(new FileReader(filePath));
+//            while ((line = br.readLine()) != null)
+//            {
+//                String[] data = line.split(",");
+//
+//                String[] answers = {data[1], data[2],data[3],data[4]};
+//                Question q = new Question(data[0], answers, data[5]);
+//                list.add(q);
+//            }
+//        }
+//        catch (FileNotFoundException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+//    
+//    
+//	
+//}
--- src/Question.java
+++ src/Question.java
@@ -1,19 +1,19 @@
-public class Question {
-
-    private String question;
-    private String answers[] = new String[4];
-    private String correctAnswer;
-
-    public Question(String question, String[] answers, String correctAnswer)
-    {
-        this.question = question;
-        this.answers = answers;
-        this.correctAnswer = correctAnswer;
-    }
-
-    public boolean isCorrect(String playerAnswer)
-    {
-        return playerAnswer == correctAnswer;
-    }
-
-}
+//public class Question {
+//
+//    private String question;
+//    private String answers[] = new String[4];
+//    private String correctAnswer;
+//
+//    public Question(String question, String[] answers, String correctAnswer)
+//    {
+//        this.question = question;
+//        this.answers = answers;
+//        this.correctAnswer = correctAnswer;
+//    }
+//
+//    public boolean isCorrect(String playerAnswer)
+//    {
+//        return playerAnswer == correctAnswer;
+//    }
+//
+//}
